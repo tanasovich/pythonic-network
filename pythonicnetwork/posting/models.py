d9ded8f9806abb83d7906c61b47e09f9d1f7805a
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2021-08-27 23:28:01 +0300

    Removed likes count field Added posted date Added user-post relationship Modified dunder str method

--- pythonicnetwork/posting/models.py
+++ pythonicnetwork/posting/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 class Post(models.Model):
-    content = models.CharField(max_length=256)
-    likes_count = models.IntegerField()
+    content = models.CharField(max_length=255)
+    posted_date = models.DateTimeField(default=timezone.now())
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
