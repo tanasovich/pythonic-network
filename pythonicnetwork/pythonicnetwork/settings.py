d4bce9baa4298e2f5a7db9ef35ec716afd4ce8b2
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2021-08-27 22:56:00 +0300

    Hidden secret key (already regenerated) Added app config to settings.py

--- pythonicnetwork/pythonicnetwork/settings.py
+++ pythonicnetwork/pythonicnetwork/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+6b*7a+nqw#e)x#r90xgnw+y+hf4bt8mgg2**eqvxfal0yc$yi'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'posting.apps.PostingConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
