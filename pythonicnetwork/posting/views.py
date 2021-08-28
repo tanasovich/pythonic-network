6221f7762f91abe9e8433dd0f9caf9fbf11c9b8b
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2021-08-28 18:04:52 +0300

    Created post and like viewsets

--- pythonicnetwork/posting/views.py
+++ pythonicnetwork/posting/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
 
-# Create your views here.
+from .models import Post, Like
+from .serializers import PostSerializer, LikeSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class LikeViewSet(viewsets.ModelViewSet):
+    queryset = Like
+    serializer_class = LikeSerializer
+    permission_classes = [permissions.IsAuthenticated]
