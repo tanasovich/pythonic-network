febcc6c3d8ca06827bcc643b2d9695b85dd1b80a
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2021-08-31 12:08:25 +0300

    Generate token during user creation

--- pythonicnetwork/posting/models.py
+++ pythonicnetwork/posting/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 from django.utils import timezone
+from rest_framework.authtoken.models import Token
 
 
 class Post(models.Model):
@@ -27,3 +31,12 @@ class Profile(models.Model):
     last_request = models.DateTimeField(default=timezone.now)
 
     objects = models.Manager()
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    print(sender)
+    print(instance)
+    if created:
+        Token.objects.create(user=instance)
+        print(Token.objects.get(user=instance))
