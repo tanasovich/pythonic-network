8144e1714753bccbc9d74a8f2bc8c994a80c324c
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2021-08-31 12:08:06 +0300

    Fixed user serializer

--- pythonicnetwork/posting/serializers.py
+++ pythonicnetwork/posting/serializers.py
@@ -31,11 +31,16 @@ class LikeSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-    password = serializers.CharField(write_only=True)
-
     class Meta:
         model = User
         fields = ['username', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(username=validated_data['username'])
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 
 class ProfileSerializer(serializers.HyperlinkedModelSerializer):
